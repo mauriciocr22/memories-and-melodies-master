14fb9773cc5abfad41acab0202343cb5b8a86c36
Maurício de Carvalho <mauriciocr223@gmail.com>
2023-06-12 21:08:56 -0300

    feat: criação do footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { CardAulas } from "./components/CardAulas";
 import { Navbar } from "./components/Navbar";
 import { Titulo } from "./components/Titulo";
 import { Historia } from "./components/Historia";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Titulo />
       <CardAulas />
       <Historia />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,54 @@
+import "./style.css";
+
+export function Footer() {
+  return (
+    <footer>
+      <div className="footer-content">
+        <div className="email-form">
+          <h1>Receber atualizações</h1>
+          <p>
+            Insira seu e-mail para receber todas as notícias e promoções
+            exclusivamente!
+          </p>
+          <form>
+            <input type="email" required />
+            <button type="submit">Enviar</button>
+          </form>
+        </div>
+        <span></span>
+        <div className="footer-lists">
+          <ul>
+            <li>
+              <a href="">Fale Conosco</a>
+            </li>
+            <li>
+              <a href="">Política de Privacidade</a>
+            </li>
+            <li>
+              <a href="">Crie sua conta</a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <a href="">Minha conta</a>
+            </li>
+            <li>
+              <a href="">Informações da Empresa</a>
+            </li>
+            <li>
+              <a href="">FAQ</a>
+            </li>
+          </ul>
+        </div>
+        <span></span>
+        <div className="contact-us">
+          <h2>Contato</h2>
+          <div>
+            <p>Av. Senador Feijó, 350 - Vila Matias, Santos</p>
+            <p>{"(13) 3227-6003"}</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
